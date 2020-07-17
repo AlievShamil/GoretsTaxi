8e7d66437b3f6ed3c1e8f90e3e7913a6a01e83cd
Shamil Aliev <inhuh005@gmail.com>
2020-07-17 22:23:46 +0300

    add DriverRegLogActivity and ConsumerRegLogActivity supplemented WelcomeActivity

---
+++ app/src/main/java/com/devcom/goretstaxi/ConsumerRegLogActivity.java
@@ -0,0 +1,14 @@
+package com.devcom.goretstaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsumerRegLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumer_reg_log);
+    }
+}
--- app/src/main/java/com/devcom/goretstaxi/WelcomeActivity.java
+++ app/src/main/java/com/devcom/goretstaxi/WelcomeActivity.java
@@ -2,13 +2,37 @@ package com.devcom.goretstaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button driverBtn, consumerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        driverBtn = findViewById(R.id.driverBtn);
+        consumerBtn = findViewById(R.id.consumerBtn);
+
+        driverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent driverIntent = new Intent(WelcomeActivity.this, DriverRegLogActivity.class);
+                startActivity(driverIntent);
+            }
+        });
+
+        consumerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent consumerIntent = new Intent(WelcomeActivity.this, ConsumerRegLogActivity.class);
+                startActivity(consumerIntent);
+            }
+        });
     }
 }
